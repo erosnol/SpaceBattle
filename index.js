9ca5fefc78e3c340e63b3fc49f410e7b99a23a5b
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-06-07 18:59:54 -0400

    added classes

---
+++ index.js
@@ -0,0 +1,63 @@
+//Create six alien ships with different properties
+//Create your ship with different properties 
+
+//* === My Ship ===
+
+class MyShip {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+
+//* =============== Alien ship numbered 1-6 =================
+
+class AlienShip1 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip2 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip3 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip4 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip5 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
+
+class AlienShip6 {
+    constructor(hull, firepower, accuracy){
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+    }
+}
